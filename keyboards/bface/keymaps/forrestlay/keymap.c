93755e7e40a67c44a1f8bade23821cb548d1d896
Benjamin Lay <benjamin@forrestlay.com>
2018-08-01 20:48:51 +1000

    Keymap: Fixed errors in personal keymap.

--- keyboards/bface/keymaps/forrestlay/keymap.c
+++ keyboards/bface/keymaps/forrestlay/keymap.c
@@ -19,8 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "action_layer.h"
 #include "rgblight.h"
 
-uint32_t rgbmode = 1
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP(
         KC_GESC,       KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,          KC_MINS,KC_EQL, KC_BSPC,
@@ -30,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,       KC_LALT,KC_LGUI,                KC_SPC,                         KC_RALT,KC_RGUI,       MO(1),  KC_RCTL
     ),
     [1] = KEYMAP(
-        KC_GRV ,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,
+        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,
         KC_TRNS,KC_TRNS,KC_UP,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_INS ,KC_HOME,KC_PGUP,KC_PSCR,KC_TRNS,KC_TRNS,
-        KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_DEL ,KC_END ,KC_PGDN,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_DEL, KC_END ,KC_PGDN,KC_TRNS,KC_TRNS,
         KC_TRNS,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,KC_TRNS,KC_TRNS,        KC_TRNS,
-        KC_TRNS,KC_TRNS, KC_TRNS,                KC_TRNS,                        KC_MPRV,KC_MPLY,KC_TRNS,KC_MNXT
+        KC_TRNS,KC_TRNS,KC_TRNS,                KC_TRNS,                        KC_MPRV,KC_MPLY,KC_TRNS,KC_MNXT
     ),
     /* You can copy this layer as base for a new fn layer * /
 	[n] = KEYMAP(
@@ -44,8 +42,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,        KC_TRNS,
         KC_TRNS,KC_TRNS,KC_TRNS,                KC_TRNS,                        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
     ), // */
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-	[0]  = ACTION_LAYER_MOMENTARY(1),
 };
