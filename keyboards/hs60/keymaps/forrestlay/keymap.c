f3b6075454235ee9d443878708139ce54a0a4a46
Benjamin Lay <benjamin@forrestlay.com>
2018-09-14 15:15:34 +1000

    Keymap: Modified personal keymaps for HS60 and Bface closer to a standard ANSI layout.

--- keyboards/hs60/keymaps/forrestlay/keymap.c
+++ keyboards/hs60/keymaps/forrestlay/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 Yiancar
+/* Copyright 2018 Benjamin Lay <benjamin@forrestlay.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,9 +21,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_60_ansi( /* Base */
     KC_GESC,  KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,            KC_MINS,  KC_EQL,  KC_BSPC,\
     KC_TAB,   KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,            KC_LBRC,  KC_RBRC, KC_BSLS,\
-    KC_LCTL,  KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     LT(1, KC_SCLN),  KC_QUOT,           KC_ENT, \
+    MO(1),    KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     LT(1, KC_SCLN),  KC_QUOT,           KC_ENT, \
     KC_LSFT,            KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,          KC_SLSH,           KC_RSFT,\
-    MO(1),    KC_LGUI,  KC_LALT,                       KC_SPC,                          KC_RALT,         KC_RGUI,  KC_APP,  KC_RCTL),
+    KC_LCTL,  KC_LGUI,  KC_LALT,                       KC_SPC,                          KC_RALT,         KC_RGUI,  KC_APP,  KC_RCTL),
 
 [1] = LAYOUT_60_ansi( /* FN */
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
@@ -43,4 +43,18 @@ void matrix_init_user(void) {
 
 void matrix_scan_user(void) {
   //user matrix
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+  switch (biton32(state)) {
+    case 0:
+      rgblight_sethsv_noeeprom_white();
+      break;
+    case 1:
+      rgblight_sethsv_noeeprom_purple();
+      break;
+    default:
+      break;
+  }
+  return state;
 }
